8cf2c016ca305e0362bff34b005f8b835135557c
Danil <danya1golman@gmail.com>
2019-10-07 19:12:37 +0700

    a

--- form/views.py
+++ form/views.py
@@ -32,7 +32,7 @@ def external(request):
 	lastJobs= run([sys.executable,'form/mongo.py',inp.encode("cp1251"),'last jobs'],shell=False,stdout=PIPE)
 
 	return render(request,'form/home.html',{
-		'Occ':inp.decode('cp1251'),
+		'Occ':inp.encode('cp1251'),
 		'Amount':Amount.stdout.decode('cp1251'),
 		'AvgSal':AvgSal.stdout.decode('cp1251'),
 		'HighestSal':HighestSal.stdout.decode('cp1251'),
