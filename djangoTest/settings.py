08f1c58761f513b0759f9e2800a07cbf0ccd682b
Danil <danya1golman@gmail.com>
2019-10-18 23:19:41 +0700

    a

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6vd2r9elk4q$g7+pxe@9==3z(1tord%d99^+u!wi6p$z0!&7ev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangotest.9kbdttkc9f.eu-central-1.elasticbeanstalk.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['djangotest.9kbdttkc9f.eu-central-1.elasticbeanstalk.com','127.0.0.1','localhost','univerityparse.azurewebsites.net']
 
 
 # Application definition
