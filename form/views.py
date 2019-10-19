ae8528d63ae41f1cc0425af3e5d5b8ccd9079991
Danil <danya1golman@gmail.com>
2019-10-19 19:13:02 +0700

    a

--- form/views.py
+++ form/views.py
@@ -236,6 +236,6 @@ def app4():
     y = list(skillsValues.values())[1:]
     div = opy.plot({
         "data":[go.Bar(x=x, y=y)],
-        "layout": go.Layout(colorway=["#4CAC40"],hovermode="closest", font=dict(family='Courier New, monospace', size=14,), margin=dict(l=40,r=0,b=200,t=30))},
+        "layout": go.Layout(colorway=["#4CAC40"],hovermode="closest", font=dict(family='Fira Sans, sans-serif', size=14,), margin=dict(l=40,r=0,b=200,t=30))},
         output_type='div')
     return div
